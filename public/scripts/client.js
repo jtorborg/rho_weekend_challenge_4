a061918a790003f685c59de2bcadbd1b5d164450
Jacquelyn Torborg <jtorborg1227@comcast.net>
2016-10-15 14:33:05 -0500

    working get and post requests; minimal styling

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,66 +1,98 @@
-console.log('inside client.js');
+//console.log('inside client.js');
 
 
 
 $(function() {
-            console.log('inside client.j doc ready');
-            //initial GET request
-            getTask();
-            //event listeners
-            $('#taskForm').on('submit', createTask);
+    //console.log('inside client.j doc ready');
+    //initial GET request
+    getTask();
+    //event listeners
+    $('#taskForm').on('submit', addTask);
 
 
 
-            //Create a front end experience that allows a user to create a Task.
-            function createTask() {
-
-                //ajax POST
-                console.log('inside createTask function');
-            }
-
-
-            function getTask() {
-
-                console.log('get task function goes here');
-                $.ajax({
-                    type: 'GET',
-                    url: '/route1',
-                    success: displayInfo
-                }); //end ajax
-            } //end getTask
+    //Create a front end experience that allows a user to create a Task.
+    function addTask(event) {
+        event.preventDefault();
+        //ajax POST
+        console.log('inside addTask function');
+        console.log("this", this);
+        var taskData = $(this).serialize();
+console.log("task Data", taskData);
+        $.ajax({
+            type: 'POST',
+            url: "/route1",
+            data: taskData,
+            success: getTask
+        }); //end of ajax post
+        $(this).find('input').val('');
 
 
+    }
 
 
-        function displayInfo(response) {
-           console.log("Response:", response);
-          //  var $list = $('.dropdownList');
-          //  $list.empty();
-          //  response.forEach(function(task) {
-          //    $list.append('<option>' + task.+ '</option>');
-          //  });
+    function getTask() {
+
+        //console.log('get task function goes here');
+        $.ajax({
+            type: 'GET',
+            url: '/route1',
+            success: displayInfo
+        }); //end ajax
+    } //end getTask
 
 
-     } //end of displayInfo function
-
-//When the Task is created, it should be stored inside of a database (SQL)
-//on server side, add created task to database
-
-//Whenever a Task is created the front end should refresh to show all tasks that need to be completed.
 
 
-//Each Task should have an option to 'Complete' or 'Delete'.
-//append complete or delete buttons to created task
+    function displayInfo(response) {
+        console.log("Response:", response);
+        var $listOfTasks = $('#taskContainer');
+        $listOfTasks.empty();
+        var $li = $('<li></li>');
+        var $form = $('<form></form>');
+        response.forEach(function(task) {
+            $form.append('<input type = "text" name = "task_name" value = "' + task.task_name + '"/>');
+            $li.append($form);
 
-//When a Task is complete, its visual representation should change on the front end. For example, the background of the task container could change from gray to green. The complete option should be 'checked off'. Each of these are accomplished in CSS, but will need to hook into logic to know whether or not the task is complete.
-//on click complete, remove class
-//Whether or not a Task is complete should also be stored in the database.
+            $listOfTasks.append($li);
+            var $updateButton = $('<button class = "updateButton">Update</button>');
+            console.log(task.id);
+            $updateButton.data('id', task.id);
 
-//database should include column for status of complete or incomplete
+            //append to actual list itself
+            $form.append($updateButton);
 
-//Deleting a Task should remove it both from the front end as well as the Database.
+            var $deleteButton = $('<button class = "deleteButton">Delete</button>');
+            $deleteButton.data('id', task.id);
+            $form.append($deleteButton);
 
-//delete should have query that removes from database
+            $li.append($form);
+            $listOfTasks.append($li);
+
+
+        }); //end of forEach
+
+
+    } //end of displayInfo function
+
+    //When the Task is created, it should be stored inside of a database (SQL)
+    //on server side, add created task to database
+
+    //Whenever a Task is created the front end should refresh to show all tasks that need to be completed.
+
+
+    //Each Task should have an option to 'Complete' or 'Delete'.
+    //append complete or delete buttons to created task
+
+    //When a Task is complete, its visual representation should change on the front end. For example, the background of the task container could change from gray to green. The complete option should be 'checked off'. Each of these are accomplished in CSS, but will need to hook into logic to know whether or not the task is complete.
+    //on click complete, remove class
+    //Whether or not a Task is complete should also be stored in the database.
+
+    //database should include column for status of complete or incomplete
+
+    //Deleting a Task should remove it both from the front end as well as the Database.
+
+    //delete should have query that removes from database
 
 
 
--- routes/route1.js
+++ routes/route1.js
@@ -29,12 +29,39 @@ router.get('/', function(req, res){
       }
 
       console.log('Got rows from the DB:', result.rows);
-      res.send(result.rows);//@@@!!! send to AJAX success 
+      res.send(result.rows);//@@@!!! send to AJAX success
     });
   });
 });//end GET request
 
 
+
+
+router.post('/', function(req, res) {
+
+    pool.connect(function(err, client, done) {
+
+            if (err) {
+              console.log('Error connecting the DB', err);
+                res.sendStatus(500);
+                done();
+                return;
+
+            } //end of if statement
+console.log("req body", req.body);
+            client.query('INSERT INTO todo (task_name) VALUES($1) returning *', [req.body.taskName], function(err, result) {
+                    done();
+                    if (err) {
+                        console.log('err', err);
+                        res.sendStatus(500);
+                        return;
+                    } //end of if
+
+                    res.send(result.rows);//!!!@@@send back to AJAX success
+                }); //end of client query
+        }); //end pool connect
+
+}); //end of router post
 
 
 
